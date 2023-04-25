cba1777391fccf4201066a527117fdc5fc369130
Luiz Maurício <2003lmvp@gmail.com>
2023-04-25 15:36:23 -0300

    update packages and linting

--- README.md
+++ README.md
@@ -2,21 +2,26 @@
 Repositório para trabalhos de grafos
 
 Para instalar as dependências:
+```python
 poetry install
+```
 
 Para execetuar:
+```python
 poetry run python graph OPTION FILE
+```
 
 Sendo OPTION susbtítuido por um número:
 
-1 - Apresenta as informações básicas do grafo
-2 - Apresenta a busca em largura
-3 - Apresenta a existência, ou não, de ciclo euleriano
-4 - Apresenta Bellman-Ford
-5 - Apresenta Floyd-Warshall
+1. Apresenta as informações básicas do grafo
+2. Apresenta a busca em largura
+3. Apresenta a existência, ou não, de ciclo euleriano
+4. Apresenta Bellman-Ford
+5. Apresenta Floyd-Warshall
 
 Sendo FILE substítuido por um arquivo de grafo num modelo como o exemplo a seguir:
 
+```
 *vertices n
 1 label1
 2 label2
@@ -29,3 +34,4 @@ label1 label2 weight1
 label1 label3 weight2
 label2 label4 weight3
 ...
+```
--- graph/t1_e5_floyd_warshall.py
+++ graph/t1_e5_floyd_warshall.py
@@ -1,17 +1,22 @@
 from graph import Graph
 
-def floyd_warshall(graph: Graph) -> list[list[int | float]]|None:
-    n = len(graph.vertices) # numero de vértices no grafo
+
+def floyd_warshall(graph: Graph) -> list[list[int | float | None]] | None:
+    n = len(graph.vertices)  # numero de vértices no grafo
     dist = []
     for i in range(n):
         dist.append([])
         for j in range(n):
-            dist[i].append(graph.peso(i+1, j+1))
-    for k in range(n): 
+            dist[i].append(graph.peso(i + 1, j + 1))
+    for k in range(n):
         for u in graph.vertices:
             for v in graph.vertices:
-                dist[u.index-1][v.index-1] = min(dist[u.index-1][v.index-1], dist[u.index-1][k]+dist[k][v.index-1])
+                dist[u.index - 1][v.index - 1] = min(
+                    dist[u.index - 1][v.index - 1],
+                    dist[u.index - 1][k] + dist[k][v.index - 1],
+                )
     return dist
+
 
 def print_floyd_warshall(graph: Graph) -> None:
     distances = floyd_warshall(graph)
